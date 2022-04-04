fff6ebe1c8854e1342958642467c49f44a41e82b
Serin-Yoon <bcode429@gmail.com>
2022-04-04 18:11:08 -0400

    tts

---
+++ tts.py
@@ -0,0 +1,26 @@
+import sys
+from gtts import gTTS
+import playsound
+import speech_recognition as sr
+
+def textToSpeech(text):
+    tts = gTTS(text=text, lang='en')
+    filename = "speech.mp3"
+    tts.save(filename)
+    playsound.playsound(filename)
+
+
+def speechToText():
+    r = sr.Recognizer
+    while(1):
+        try:
+            with sr.Microphone() as source:
+                r.adjust_for_ambient_noise(source, duration=0.2)
+                audio = r.listen(source)
+                text = r.recognize_google(audio)
+                text = text.lower()
+                print("Text:", text)
+        except sr.RequestError as e:
+            print(e)
+        except sr.UnknownValueError:
+            print("unknown error")
