2b2ad4d0d2ae91f515a298272bbc2c7899a7c812
JacobDesilets <jacobdesilets@gmail.com>
2022-04-07 21:04:06 -0400

    Added some stuff, no major changes.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import sys, cv2
 
-from PyQt6.QtCore import QThread, pyqtSignal, Qt
-from PyQt6.QtGui import QImage, QPixmap
+from PyQt6.QtCore import QThread, pyqtSignal, Qt, QRect
+from PyQt6.QtGui import QImage, QPixmap, QPainter
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
 
 # face recognition
 detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
+
+        self.face_rect = None
 
         self.layout = QVBoxLayout()
 
@@ -19,21 +22,30 @@ class MainWindow(QMainWindow):
         self.feed_label = QLabel()
         self.feed_label.setPixmap(QPixmap('nodata.png'))
         self.layout.addWidget(self.feed_label)
+        self.painter = QtGui.QPainter(self.feed_label.pixmap())
 
-        self.cancel_btn = QPushButton()
+        self.cancel_btn = QPushButton("Take photo")
         self.cancel_btn.clicked.connect(self.cancel_feed)
         self.layout.addWidget(self.cancel_btn)
 
         self.webcam_thread = WebcamWorker()
         self.webcam_thread.start()
         self.webcam_thread.img_update.connect(self.img_update_slot)
+        self.webcam_thread.face_update.connect(self.face_update_slot)
 
         widget = QWidget()
         widget.setLayout(self.layout)
         self.setCentralWidget(widget)
 
+    def draw_face_rect(self):
+
+
     def img_update_slot(self, img):
         self.feed_label.setPixmap(QPixmap.fromImage(img))
+
+    def face_update_slot(self, rect):
+        self.face_rect = rect
+        self.draw_face_rect()
 
     # activate after receiving position from the user
     def cancel_feed(self):
@@ -42,6 +54,7 @@ class MainWindow(QMainWindow):
 
 class WebcamWorker(QThread):
     img_update = pyqtSignal(QImage)
+    face_update = pyqtSignal(QRect)
 
     def run(self):
         self.thread_active = True
@@ -54,7 +67,8 @@ class WebcamWorker(QThread):
                 flipped_img = cv2.flip(img, 1)
                 faces = detector.detectMultiScale(flipped_img, 1.3, 5)
                 for (x, y, w, h) in faces:
-                    cv2.rectangle(flipped_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                    #cv2.rectangle(flipped_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                    self.face_update.emit(QRect(x, y, w, h))
                 output = QImage(flipped_img.data, flipped_img.shape[1], flipped_img.shape[0], QImage.Format.Format_RGB888).scaled(640, 480, Qt.AspectRatioMode.KeepAspectRatio)
             else:
                 output = QImage.load('nodata.png')
@@ -64,8 +78,8 @@ class WebcamWorker(QThread):
 
     def stop(self):
         #self.img_update.emit(QImage.load('nodata.png'))
-        self.thread_active = False
-        self.quit()
+        #self.thread_active = False
+        #self.quit()
 
         selfie = cv2.flip(self.frame, 1)
         cv2.imwrite('selfie.png', selfie)
